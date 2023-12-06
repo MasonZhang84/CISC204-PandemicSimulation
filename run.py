a29681c7d3786d3a27353dc2dffa347efdcdbcb1
MasonZhang84 <149118840+MasonZhang84@users.noreply.github.com>
2023-12-05 20:54:02 -0500

    code

--- run.py
+++ run.py
@@ -1,29 +1,10 @@
-import math
-import random
 
 from bauhaus import Encoding, proposition, constraint
 from bauhaus.utils import count_solutions, likelihood
-from nnf import Var
 
 # These two lines make sure a faster SAT solver is used.
 from nnf import config
-
 config.sat_backend = "kissat"
-
-# Encoding that will store all of your constraints
-E = Encoding()
-
-agents = [(True, False, False, 0, 0, 0), # alive, not infected, not immune
-          (True, True, False, 0, 0, 0), # alive, infected, not dead
-          (False, True, False, 0, 0, 0),# not alive, infected, not immune
-          (True, False, True, 0, 0, 0)]# alive, not infected, immune
-
-ToF = (True, False)
-
-# universal constants
-maxTime = 3
-planeMaxX = 2
-planeMaxY = 2
 
 class Hashable:
     def __hash__(self):
@@ -35,80 +16,101 @@ class Hashable:
     def __repr__(self):
         return str(self)
 
+# Encoding that will store all of your constraints
+E = Encoding()
 
-#repersents a individal in our simulation
+# To create propositions, create classes for them first, annotated with "@proposition" and the Encoding
 @proposition(E)
-class agent_node(Hashable):
-    def __init__(self, alive, infected, immune):
+class BasicPropositions:
+
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return f"A.{self.data}"
+
+
+# Different classes for propositions are useful because this allows for more dynamic constraint creation
+# for propositions within that class. For example, you can enforce that "at least one" of the propositions
+# that are instances of this class must be true by using a @constraint decorator.
+# other options include: at most one, exactly one, at most k, and implies all.
+# For a complete module reference, see https://bauhaus.readthedocs.io/en/latest/bauhaus.html
+@constraint.at_least_one(E)
+@proposition(E)
+class FancyPropositions:
+
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return f"A.{self.data}"
+
+# Call your variables whatever you want
+a = BasicPropositions("a")
+b = BasicPropositions("b")
+c = BasicPropositions("c")
+d = BasicPropositions("d")
+e = BasicPropositions("e")
+# At least one of these will be true
+x = FancyPropositions("x")
+y = FancyPropositions("y")
+z = FancyPropositions("z")
+
+
+# Build an example full theory for your setting and return it.
+#
+#  There should be at least 10 variables, and a sufficiently large formula to describe it (>50 operators).
+#  This restriction is fairly minimal, and if there is any concern, reach out to the teaching staff to clarify
+#  what the expectations are.
+def example_theory():
+    # Add custom constraints by creating formulas with the variables you created.
+    E.add_constraint((a | b) & ~x)
+    # Implication
+    E.add_constraint(y >> z)
+    # Negate a formula
+    E.add_constraint(~(x & y))
+    # You can also add more customized "fancy" constraints. Use case: you don't want to enforce "exactly one"
+    # for every instance of BasicPropositions, but you want to enforce it for a, b, and c.:
+    constraint.add_exactly_one(E, a, b, c)
+
+    return E
+
+
+
+
+if __name__=="__main__":
+    print('hello')
+    T = example_theory()
+    # Don't compile until you're finished adding all your constraints!
+    T = T.compile()
+    # After compilation (and only after), you can check some of the properties
+    # of your model:
+    print("\nSatisfiable: %s" % T.satisfiable())
+    print("# Solutions: %d" % count_solutions(T))
+    print("   Solution: %s" % T.solve())
+
+    print("\nVariable likelihoods:")
+    for v, vn in zip([a,b,c,x,y,z], 'abcxyz'):
+        # Ensure that you only send these functions NNF formulas
+        # Literals are compiled to NNF here
+        print(" %s: %.2f" % (vn, likelihood(T, v)))
+    print()
+
+rows, cols = (3, 3)
+arr = [[0]*cols]*rows
+
+class plane:
+    def __init__(self):
+        agentlist = []
+
+    def addAgents(self, newAgent):
+        self.agentlist.append(newAgent)
+
+
+class agent:
+    def __init__(self, name, infected, alive, immune):
+        self.name = name
         self.infected = infected
         self.alive = alive
         self.immune = immune
 
-    def __repr__(self):
-        return f'[alive = {self.alive}, infected = {self.infected}, immune = {self.immune}\n]'
-
-
-#repersents an individal's time and space
-@proposition(E)
-class timeAndLocation(Hashable):
-    def __init__(self, agent, time, x, y):
-        self.x = x
-        self.y = y
-        self.time = time
-        self.agent = agent
-
-        def __repr__(self):
-            return f'[node: {self.agent}\n time: {self.time}, location: ({self.x},{self.y})]'
-
-
-# this will be true if the population is guaranteed to die
-@proposition(E)
-class populationDeath(Hashable):
-    def __init__(self):
-        pass
-
-    def __repr__(self):
-        return f'[The population will die]'
-
-
-
-#Initalize Proposition
-agentsProposition = []
-
-for i in range(len(agents)):
-                for alive in ToF:
-                    for infected in ToF:
-                        for immune in ToF:
-                            agentsProposition.append(agent_node(alive, infected, immune))
-
-agentTimeAndSpace = []
-for agent in agentsProposition:
-    for t in maxTime:
-        for x in planeMaxX:
-            for y in planeMaxY:
-                agentTimeAndSpace.append(timeAndLocation(agent, t, x, y))
-
-demise = populationDeath()
-
-# constraints
-def test_theory():
-    #agent cannot be infected and immune
-
-    for agent in agentsProposition:
-        # agent cannot be infected and immune at the same time
-        # agent_node(alive, infected, immune)
-        E.add_constraint(~((agent_node(True, False, True)|agent_node(False, False, True))&
-                           (agent_node(True, True, False)|agent_node(False, True, False))))
-
-    return E
-if __name__ == "__main__":
-
-    #applie restriction and compile
-    T = test_theory()
-    T = T.compile()
-
-    print("\nSatisfiable: %s" % T.satisfiable())
-
-    print("# Solutions: %d" % count_solutions(T))
-
-    print("   Solution: %s" % T.solve())
