bfeef73540b6b99e0860e9a5b4d1398e36425224
camwalten <camwalten@gmail.com>
2023-12-07 18:24:54 -0500

    FINAL
    Comments and adjustments for FINAL project

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import random
 
 from bauhaus import Encoding, proposition, constraint
 from bauhaus.utils import count_solutions, likelihood
-from nnf import Var
+
 
 
 # These two lines make sure a faster SAT solver is used.
@@ -14,24 +14,35 @@ config.sat_backend = "kissat"
 # Encoding that will store all of your constraints
 E = Encoding()
 
-agents = [(True, False,0,0),
+"""
+The agents list and the universal constants act as ways for the user to change certain properties that the code runs on.
+The agents is a list of all the agents in the given simulation. (agents = (Alive,Infected,x-coord,y-coord))
+The maxTime controls, how many time steps the problem runs for (# of loops ran).
+The planeMaxX controls how many x tiles are in the plane. 
+The planeMaxY controls how many y tiles are in the plane. 
+The Lethality controls how likely the infection is to kill on the next turn.
+"""
+
+agents = [(True, False, 0,0),
           (True, False, 0,0),
           (False, False, 2,2),
-          (True, True, 3,3),
+          (True, True, 2,2),
           (True, True, 0,1),
           (False, True, 2, 2),
-          (True, True, 3, 3)]
-
-
-ToF = (True, False)
+          (True, True, 2, 2)]
 
 # universal constants
-maxTime = 200
+maxTime = 10
 planeMaxX = 3
 planeMaxY = 3
 lethality = 2
 
+
 class Hashable:
+    """
+    This class is inherited from the prof's example and is used to help print out the code.
+
+    """
     def __hash__(self):
         return hash(str(self))
 
@@ -42,9 +53,13 @@ class Hashable:
         return str(self)
 
 
-#repersents a individal in our simulation
+#repersents an individal in our simulation
 @proposition(E)
 class agent_node:
+    """
+    This class controls each individual character node which has if the agent is alive or infected. This also helps
+    with printouts.
+    """
     def __init__(self, alive, infected):
         self.infected = infected
         self.alive = alive
@@ -56,6 +71,10 @@ class agent_node:
 #repersents an individal's time and space
 @proposition(E)
 class timeAndLocation:
+    """
+    This class controls each agent with their individual time and space, this class helps with the time steps and move
+    ment of agents. This also helps with printouts.
+    """
     def __init__(self, agent, time, x, y):
         self.x = x
         self.y = y
@@ -69,14 +88,26 @@ class timeAndLocation:
 # this will be true if the population is guaranteed to die
 @proposition(E)
 class populationDeath:
+    """
+    This is currently unused as the infection does not seem to work properly, this was meant to tell the user if the
+    population succumbed to the disease or not. This also helps with printouts.
+    """
     def __init__(self):
         pass
 
     def __repr__(self):
         return f'[The population will die]'
 
-#(self, agent, time, x, y)
+#(self, agent, time, x, y) #this works!
 def move_agent(agent):
+    """
+    This function controls the movement of each agent in the given simulation, using the timeAndLocation class defined
+    above. This fucntion works assinging a possible x and y value that an agent can move to and moves the agent to that
+    x or y as long as it is still inside the plane as defined by the user. (We use math.random to pick which space
+    to move next)
+    :param agent: An agent in timeAndSpace that has not been moved yet.
+    :return: An agent in timeAndSpace that has been moved.
+    """
 
     temp = timeAndLocation(agent.agent, agent.time, agent.x, agent.y)
 
@@ -107,84 +138,106 @@ def move_agent(agent):
 
         return temp
 
-def infect(agents):
-    infect = False
+def infect(curState):
+    """
+    This is currently bugged and does not work. (See bug exploration in final write-up for details)
+
+    This function is supposed to go through the currentState and then check if healthy nodes are in the same tile
+    as infected ones and then make both be infected.
+
+    :param curState: The current Game state with all nodes inside of it.
+    :return: GameState with infected healthy nodes that are supposed to be infected.
+    """
 
     infectZone = []
 
-    for i in agents:
+    for i in curState:
         if i.agent.infected == True:
-            infect = True
             if (i.x, i.y) not in infectZone:
                 infectZone.append((i.x, i.y))
 
     if infect:
         for zone in infectZone:
-            for i in agents:
+            for i in curState:
                 if i.agent.alive == True:
                     if i.x == zone[0] and i.y == zone[1]:
                         i.agent.infected = True
 
+
+
 def death(population, t):
+    """
+    This checks if the final game state is fully dead (or full of infected agents) and prints out if it is. This
+    currently does not work as infection does not, but it should work if infection fully worked.
+    :param population: Takes in the final game state.
+    :param t: a time step in time and space.
+    :return:
+    """
     for i in range(len(population[-1])):
         if population[-1][i].agent.infected == True:
             if t >= lethality and population[t-lethality][i].agent.infected == True:
                 population[-1][i].alive = False
+                print("Population is fully dead.")
 
 
-print("tyest")
-
-#Initalize Proposition
+"""
+This next bit of code, puts all the agent nodes into the time and spaces and creates the current game state.
+"""
 agentsProposition = []
-simulatuionStates = []
+simulationStates = []
 
 currentState = []
 
 for i in agents:
-    agentsProposition.append(agent_node(i[0],i[1]))
-    currentState.append(timeAndLocation(agent_node(i[0],i[1]),0,i[2], i[3]))
+    agentsProposition.append(agent_node(i[0], i[1]))
+    currentState.append(timeAndLocation(agent_node(i[0], i[1]), 0, i[2], i[3]))
+    
 
-simulatuionStates.append(currentState)
+simulationStates.append(currentState)
 
 demise = populationDeath()
 
 # constraints
 def test_theory():
-
+    """
+    This is our propositions for the problem.
+    :return: E which adds constraints on agents.
+    """
     #agent cannot be infected and immune
     for agent in agentsProposition:
         # agent cannot be infected and immune at the same time
         # agent_node(alive, infected, immune)
-        E.add_constraint(~((agent_node(True, False, True)|agent_node(False, False, True))&
-                           (agent_node(True, True, False)|agent_node(False, True, False))))
+        E.add_constraint(~((agent_node(True, False, True) | agent_node(False, False, True)) &
+                           (agent_node(True, True, False) | agent_node(False, True, False))))
 
     return E
 
 if __name__ == "__main__":
-
+    """
+    This is where are main bit of code is located. This code runs until the user specified time and just keeps 
+    doing moving all the agents, infecting them and checking if the population has died.
+    """
     t = 1
 
     while t <= maxTime:
         currentState = []
 
-        infect(currentState)
-
-        #move agent
-        for agent in simulatuionStates[t-1]:
+        #move agent and if they share cell, infect everyone in the cell
+        for agent in simulationStates[t - 1]:
             currentState.append(move_agent(agent))
 
-        # if they share cell, infect everyone in the cell
+        infect(currentState)
 
-        death(simulatuionStates, t)
+        death(simulationStates, t)
 
-        simulatuionStates.append(currentState)
+        simulationStates.append(currentState)
         # increase time
         t = t + 1
 
+    print("There are", len(simulationStates), "Simulation States!")
 
-
-    print(len(simulatuionStates))
-    for i in simulatuionStates:
+    #correct way to print out our final game state
+    for i in simulationStates:
         print("--------------------------------------")
         for j in i:
             print(j)
